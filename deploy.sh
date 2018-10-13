b13bd41889c7b6eb347c3ca9f338c3b5860d3d27
Webia1 <erkan.buelbuel@gmail.com>
2018-10-13 23:36:23 +0200

    restore old deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 # abort on errors
 set -e
 
+# delete dist
+rm -rf docs/.vuepress/dist
+
 # build
 npm run build
 
@@ -16,6 +19,6 @@ git init
 git add -A
 git commit -m 'deploy'
 git remote add origin https://github.com/webia1/webia1.github.io.git
-git push -u origin master
+git push -u origin master -f
 
 cd -
